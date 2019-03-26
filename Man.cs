229e35da2a6f003f3a68e32d36778027c584a047
Иргалиев Ербол Жумагалиевич <@corp.itstep>
2019-03-26 17:35:23 +0600

    first commit

---
+++ Man.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsPractice
+{
+    public delegate void PropertyEventHandler(object sender, Man e);
+    public class Man : IPropertyChanged
+    {
+        public string Name { get; set; } = "Oleg";
+        public bool isHandsome;
+        public bool IsHandsome { get
+            {
+                return isHandsome;
+            }
+            set
+            {
+                PropertyChanged?.Invoke(Name, this);
+                isHandsome = value;
+            }
+        }
+
+        public event PropertyEventHandler PropertyChanged;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Man man = new Man
+            {
+                IsHandsome = true
+            };
+            man.PropertyChanged += (sender, element) => Console.WriteLine($"{sender} has been changed property of class {element}");
+            man.IsHandsome = false;
+            Console.ReadLine();
+        }
+    }
+}
